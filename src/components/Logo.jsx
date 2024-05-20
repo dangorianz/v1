a76cc491be171f1acaf6db73ae367d6f78c30fbc
Daniel Gorianz <daniel.gorianz@qallta.com>
2024-05-20 17:11:22 -0400

    reponsive header done

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo = () => {
     return (
-      <div className="px-5 py-4 bg-transparent sm:hidden w-full z-10">
+      <div className="px-5 py-4 bg-transparent w-full z-10">
           <svg width="70" height="70" viewBox="0 0 100 115" className="cursor-pointer absolute z-10 -translate-x-1 -translate-y-1 transition-all duration-300">
               <polygon points="50,5 95,30 95,80 50,105 5,80 5,30" fill="#0f172a" stroke="#64ffda" strokeWidth="7" />
               <text x="50%" y="50%" textAnchor="middle" fill="#64ffda" fontSize="45" fontFamily="Arial" dy=".3em">DG</text>
--- src/pages/index.js
+++ src/pages/index.js
@@ -63,20 +63,20 @@ const IndexPage = () => {
       <Mail/>
       <NavBar />
       {/* <div className="text-white min-h-screen lg:px-40 xl:px-72 lg:flex justify-between items-center"> */}
-      <div className="text-white min-h-screen pt-40 sm:pt-0 sm:flex items-center px-5 sm:px-32 md:40">
-          <div className="flex flex-col justify-center items-center sm:items-start pb-20">
-            <p className='text-teal-300 animate__animated animate__fadeInDown animate__slow'><Logo/></p>
+      <div className="text-white min-h-screen pt-40 sm:pt-0 sm:flex md:justify-between 2xl:px-72 items-center px-5 sm:px-32 md:40">
+          <div className="flex flex-col justify-center items-center md:items-start pb-20">
+            <p className='text-teal-300 md:hidden animate__animated animate__fadeInDown animate__slow'><Logo/></p>
             <p className="mt-5 text-5xl xl:text-6xl font-bold text-slate-300 animate__animated animate__fadeInDown animate__slow"><span className="text-teal-300">D</span>aniel <span className="text-teal-300">G</span>orianz</p>
             <p className='mt-5 text-3xl sm:text-4xl xl:text-4xl font-bold text-[#8892b0] animate__animated animate__fadeInDown animate__slow subtitle-animation'>FullStack Developer</p>
-            <p className="mt-10 text-center sm:text-start sm:max-w-[500px] animate__animated animate__fadeInDown animate__slow description-animation">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
+            <p className="mt-10 text-center md:text-start max-w-[500px] animate__animated animate__fadeInDown animate__slow description-animation">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
             <Link className="w-fit animate__animated animate__fadeInDown animate__slow description-animation" download href='https://drive.google.com/file/d/1RNVOjaeZqsgsP0m6jCvx5R9LqmTw5WRp/view?usp=sharing' target='_blank'>
                 <div className='border-teal-300 bg-[#111827] text-teal-300 mt-10 rounded border-2 px-5 py-2 transition-all duration-300 shadow-hover hover:-translate-x-1 hover:-translate-y-1'>
                   Resume
                 </div>
             </Link>
           </div>
-          <div className="hidden md:block">
-            <StaticImage src="../images/dalle2.webp" alt="logo 2" class="hidden md:block md:w-[650px] animate__animated " />
+          <div className="hidden custom:block bg-red">
+            <StaticImage src="../images/dalle2.webp" alt="logo 2" class="w-full max-w-xl mx-auto animate__animated" />
           </div>
       </div>
     </main>

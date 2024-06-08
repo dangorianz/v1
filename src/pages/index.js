af8410ab175424fd30c1133d2a4c5da6931b160d
Daniel Gorianz <daniel.gorianz@qallta.com>
2024-06-08 01:18:38 -0400

    change resume url

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,6 @@ const IndexPage = () => {
 
   return (
     <main className="h-screen relative">
-      
-    
       <SocialNetwork/>
       <Mail/>
       <NavBar />
@@ -18,7 +16,7 @@ const IndexPage = () => {
             <p className="mt-5 text-5xl sm:text-6xl md:text-7xl font-bold text-slate-100 animate__animated animate__fadeInDown animate__slow"><span className="text-teal-300">D</span>aniel <span className="text-teal-300">G</span>orianz</p>
             <p className='mt-5 text-3xl sm:text-4xl font-bold text-[#b7bdcf] animate__animated animate__fadeInDown animate__slow subtitle-animation'>FullStack Developer</p>
             <p className="mt-10 text-center max-w-[500px] animate__animated animate__fadeInDown animate__slow description-animation">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
-            <a className="w-fit animate__animated animate__fadeInDown animate__slow description-animation cursor-pointer" href='https://drive.google.com/file/d/1RNVOjaeZqsgsP0m6jCvx5R9LqmTw5WRp/view?usp=sharing' target='_blank' rel="noreferrer">
+            <a className="w-fit animate__animated animate__fadeInDown animate__slow description-animation cursor-pointer" href='https://drive.google.com/file/d/1g11N2-cxs_61qLIGUrEeuq8PTEFIrXub/view?usp=share_link' target='_blank' rel="noreferrer">
                 <div className='border-teal-300 bg-[#111827] text-teal-300 mt-10 rounded border-2 px-5 py-2 transition-all duration-300 shadow-hover hover:-translate-x-1 hover:-translate-y-1'>
                   Check Resume
                 </div>
@@ -26,7 +24,6 @@ const IndexPage = () => {
           </div>
       </div>
       <Experience />
-      
     </main>
   )
 }
